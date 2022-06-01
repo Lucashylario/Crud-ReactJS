f7e2cf10f8126511b5cb2430a7e2c6a185eda3e1
Lucashylario <lucashyfrotend@hotmail.com>
2022-06-01 18:37:04 -0300

    ajuste form

--- README.md
+++ README.md
@@ -5,11 +5,7 @@
 - TailwindCSS
 
 **Back end**
-- Node
-- MySQL
-- Express
-- Cors
-- Nodemon
+
 
 ## Executar para abrir o projeto
 
@@ -18,7 +14,7 @@
 ## Rodar Front end & Back end
 `npm run dev` - Para rodar as aplicaçõe, deve-se entrar na pasta de back, ativar o terminal e digitar o cód. Fazer o mesmo com a pasta do Front.
 
-## Vídeo aula útilizada como base para o projeto.
-- 1. Criação de impunts e servidor para os dados = `https://www.youtube.com/watch?v=e0He6sCiQT8&t=622s`
-
-- 2. Conexão com banco de dados MySQL = `https://www.youtube.com/watch?v=5_9rvyT9cg4`
+## Vídeo aula utilizada como base para o projeto.
+- Canal: ProgrAmada Mente
+- 1. REACTjs + BOOTSTRAP 5 - CRUD Completo - Parte 01
+`https://www.youtube.com/watch?v=4sQhe7SEWsU`
--- pagina-cadastro/src/components/Product/index.jsx
+++ pagina-cadastro/src/components/Product/index.jsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from 'react';
-import Axios from 'axios';
-import { CardProducts } from './CardProducts';
 
 export const Product = () => {
     return (
@@ -11,13 +8,12 @@ export const Product = () => {
             </header>
 
             <div className="w-[95%] h-3/5 p-10 mx-auto mt-5 bg-slate-100 shadow-lg">
-                <div>
+                <div className="grid gap-3 justify-center">
                     <input 
                         type="text" 
                         name="name" 
                         placeholder="Nome" 
                         className='bg-slate-200 w-96 p-3 text-xl rounded-2xl border-2 border-slate-500' 
-                        onChange={handleChangeValue} 
                     />
 
                     <input 
@@ -25,7 +21,6 @@ export const Product = () => {
                         name="price" 
                         placeholder="Preço" 
                         className='bg-slate-200 w-96 p-3 text-xl rounded-2xl border-2 border-slate-500' 
-                        onChange={handleChangeValue} 
                     />
 
                     <input 
@@ -33,25 +28,21 @@ export const Product = () => {
                         name="marca" 
                         placeholder="Marca" 
                         className='bg-slate-200 w-96 p-3 text-xl rounded-2xl border-2 border-slate-500' 
-                        onChange={handleChangeValue} 
                     />
                 
                     <button 
-                        className='bg-slate-900 text-white px-10 py-3 rounded-3xl' 
-                        onClick={() => handleClickButton()}>Cadastrar</button>
-                
+                        className='
+                            w-80 
+                            bg-slate-900 
+                            mx-auto 
+                            text-white 
+                            px-10 
+                            py-3 
+                            rounded-3xl 
+                            hover:bg-blue-900
+                            '
+                    >Cadastrar</button>
                 </div>
-                { typeof listProducts !== "undefined" && listProducts.map((values) => {
-                    return <CardProducts 
-                        key={value.id} 
-                        listCard={listProducts} 
-                        setListCard={setListProducts} 
-                        id={value.id}
-                        name={value.name}
-                        price={value.price}
-                        marca={value.marca}
-                        />;
-                })}
             </div>
         </div>
     )

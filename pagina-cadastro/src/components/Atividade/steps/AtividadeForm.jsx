1464e36a8ab6df409efd617b4256beba8f54f6ef
Lucashylario <lucashyfrotend@hotmail.com>
2022-06-16 15:45:07 -0300

    add botão com função de cancelar edição ativ.

--- pagina-cadastro/src/components/Atividade/steps/AtividadeForm.jsx
+++ pagina-cadastro/src/components/Atividade/steps/AtividadeForm.jsx
@@ -24,6 +24,7 @@ export const AtividadeForm = (props) => {
         setAtividade({...atividade, [name]: value})
     }
 
+    // Função para selecionar atividade para editar
     function atividadeAtual() {
         if (props.ativSelecionada.id !== 0) {
             return props.ativSelecionada;
@@ -31,6 +32,15 @@ export const AtividadeForm = (props) => {
         else {
             return atividadeInicial;
         }
+    }
+
+    // Método do botão de cancelar atividade de edição da atividade no form
+    const handleCancelar = (e) => {
+        e.preventDefault();
+
+        // cancelaAtividade()
+
+        setAtividade(atividadeInicial);
     }
 
     // Estrutura do formulário
@@ -60,21 +70,62 @@ export const AtividadeForm = (props) => {
             </div>
             
             <div className="w-full flex justify-center">
-                <button 
-                    onClick={props.addAtividade} 
-                    className="
-                        w-2/5
-                        max-w-sm
-                        bg-slate-200 
-                        text-slate-800 border 
-                        border-slate-800 
-                        hover:bg-slate-800
-                        hover:text-slate-200
-                        hover:shadow-md
-                        hover:shadow-slate-800
-                        py-2 
-                        rounded-lg
-                ">+ Atividade</button>
+                {
+                    atividade.id === 0 ? // Se o id for igual á 0 aparece o botão abaixo,
+                    (<button 
+                        onClick={props.addAtividade} 
+                        className="
+                            w-2/5
+                            max-w-sm
+                            bg-slate-200 
+                            text-slate-800 border 
+                            border-slate-800 
+                            hover:bg-slate-800
+                            hover:text-slate-200
+                            hover:shadow-md
+                            hover:shadow-slate-800
+                            py-2 
+                            rounded-lg
+                    ">+ Atividade</button>)
+                    : // Se não aparece os botões abaixo
+                    (<>
+                        <button 
+                            type="submit"
+                            className="
+                                w-2/5
+                                max-w-sm
+                                mx-2
+                                font-semibold
+                                bg-slate-200 
+                                text-green-600 border 
+                                border-green-600 
+                                hover:bg-green-600
+                                hover:text-slate-200
+                                hover:shadow-md
+                                hover:shadow-slate-800
+                                py-2 
+                                rounded-lg
+                        ">Salvar</button>
+
+                        <button 
+                            onClick={handleCancelar} 
+                            className="
+                                w-2/5
+                                max-w-sm
+                                mx-2
+                                font-semibold
+                                bg-slate-200 
+                                text-red-600 border 
+                                border-red-600 
+                                hover:bg-red-600
+                                hover:text-slate-200
+                                hover:shadow-md
+                                hover:shadow-slate-800
+                                py-2 
+                                rounded-lg
+                        ">Cancelar</button>
+                    </>)
+                }
             </div>
         </form>
     )
